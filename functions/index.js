a483f1aeaa5b4693120a8276fda5189883555364
kennethzhang31 <100484973+kennethzhang31@users.noreply.github.com>
2024-06-22 12:33:11 +0800

    fixed cloud functions

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,26 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch'); 
+
+const importDynamic = (modulePath) => import(modulePath);
+
+let gradioLoaded = false;
+
+// importDynamic('@gradio/client').then((module) => {
+//   const { client } = module;
+//   gradioLoaded = true;
+// }).catch(error => {
+//   console.error('Failed to load module:', error);
+//   gradioLoaded = false;
+// });
+
+const app = express();
+app.use(cors({ origin: true }));
+app.use(express.json());
+
 function generateIdempotencyKey(userId) {
     return `createUserDocument_${userId}`;
 }
@@ -47,3 +67,46 @@ exports.createUserDocument = functions.auth.user().onCreate(async (user) => {
         throw new functions.https.HttpsError('internal', 'Unable to create user document');
     }
 });
+
+app.get('/add', (req, res) => {
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        return res.status(400).send('Invalid input');
+    }
+
+    const c = a + b;
+    res.send({ result: "gradioLoaded " + (gradioLoaded ? "Yay" : "Nay") + c});
+});
+
+app.get('/api/virtual-tryon', async (req, res) => {
+    const { backgroundUrl, garmentUrl } = req.query;
+
+    // Fetching the garment image as a blob
+    const garmentResponse = await fetch(garmentUrl);
+    const garmentBlob = await garmentResponse.blob();
+
+    const backgroundResponse = await fetch(backgroundUrl);
+    const backgroundBlob = await backgroundResponse.blob();
+
+    // Initialize the Gradio client
+    const { client } = await import('@gradio/client');
+    const gradioApp = await client("yisol/IDM-VTON");
+
+    // Predict using the Gradio model
+    try{
+        const result = await gradioApp.predict("/tryon", [
+            {"background": backgroundBlob, "layers":[], "composite": null},
+            garmentBlob,
+            "wear", true, true, 20, 20
+        ]);
+        res.status(200).json({ result: result.data });
+    }
+    catch(e){
+        res.status(500).send({ result: e });
+    }
+});
+
+// Export the API to Firebase Cloud Functions
+exports.api = functions.https.onRequest(app);
