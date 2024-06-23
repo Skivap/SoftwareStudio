fce5f7a98e7b9509bd1cb28e343ebaa3ef5ff065
brandon <blouis693@gmail.com>
2024-06-23 18:04:14 +0800

    fix bug

--- lib/widgets/product.dart
+++ lib/widgets/product.dart
@@ -52,212 +52,7 @@ class _ProductState extends State<ProductContent> {
     isLiked = widget.productData.likedby.contains(FirebaseAuth.instance.currentUser?.uid);
     getUserInfo(widget.productData.sellerID);
   }
-  Future<void> pickImage(StateSetter setModalState) async {
-    final ImagePicker _picker = ImagePicker();
-    final XFile? pickedImage = await _picker.pickImage(source: ImageSource.gallery);
-
-    if (pickedImage != null) {
-      if (universal_io.Platform.isAndroid || universal_io.Platform.isIOS) {
-        setModalState(() {
-          _pickedImage = io.File(pickedImage.path);
-          
-        });
-      } else {
-        String? uploadedImageUrl = await uploadImageWeb(pickedImage);
-        setModalState(() {
-          _imageUrl = uploadedImageUrl;
-        });
-      }
-      
-      
-      
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('No image selected')),
-      );
-    }
-  }
-
-  Future<String?> uploadImage(io.File image) async {
-    try {
-      String filename = DateTime.now().millisecondsSinceEpoch.toString();
-      Reference storageRef = FirebaseStorage.instance.ref().child('product_images/$filename');
-      UploadTask uploadTask = storageRef.putFile(image);
-      TaskSnapshot taskSnapshot = await uploadTask;
-      return await taskSnapshot.ref.getDownloadURL();
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error uploading image: $e')),
-      );
-      return null;
-    }
-  }
-
-  Future<String?> uploadImageWeb(XFile image) async {
-    try {
-      String filename = DateTime.now().millisecondsSinceEpoch.toString();
-      Reference storageRef = FirebaseStorage.instance.ref().child('product_images/$filename');
-      UploadTask uploadTask = storageRef.putData(await image.readAsBytes());
-      TaskSnapshot taskSnapshot = await uploadTask;
-      return await taskSnapshot.ref.getDownloadURL();
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error uploading image: $e')),
-      );
-      return null;
-    }
-  }
-
-void addProduct() async {
-    String productName = _productNameController.text.trim();
-    String productPrice = _productPriceController.text.trim();
-    String productDescription = _productDescriptionController.text.trim();
-    String productLink = _productLinkUrlController.text.trim();
-    User? user = FirebaseAuth.instance.currentUser;
-    String? userId = user?.uid ?? '';
-
-    if (productName.isNotEmpty &&
-        productPrice.isNotEmpty &&
-        productDescription.isNotEmpty &&
-        (_pickedImage != null || _imageUrl != null) &&
-        productLink.isNotEmpty) {
-      try {
-        String? imageUrl;
-        if (_pickedImage != null) {
-          imageUrl = await uploadImage(_pickedImage!);
-        } else {
-          imageUrl = _imageUrl;
-        }
-
-        if (imageUrl != null) {
-         await _firestore.collection('products').add({
-            'sellerId': userId,
-            'name': productName,
-            'price': double.parse(productPrice),
-            'description': productDescription,
-            'imageUrl': imageUrl,
-            'link': productLink,
-            'likedby': [],
-            'likes':0,
-            'comment_count': 0,
-          });
-          
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Product Added Successfully')),
-          );
-          Navigator.pop(context);
-
-          _productNameController.clear();
-          _productPriceController.clear();
-          _productDescriptionController.clear();
-          _productLinkUrlController.clear();
-          setState(() {
-            _pickedImage = null;
-            _imageUrl = null;
-          });
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Failed to upload Image')),
-          );
-        }
-      } catch (e) {
-        print('Error adding product: $e');
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to add product: $e')),
-        );
-      }
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please fill in all fields')),
-      );
-    }
-  }  
-
-void showProductForm(BuildContext context) {
-  showModalBottomSheet<dynamic>(
-    isScrollControlled: true,
-    context: context,
-    builder: (context) {
-      return StatefulBuilder(
-        builder: (BuildContext context, StateSetter setModalState) {
-          return FractionallySizedBox(
-            heightFactor: 0.9,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  if (_pickedImage == null && _imageUrl == null)
-                    GestureDetector(
-                      onTap: () => pickImage(setModalState),
-                      child: Container(
-                        height: 250,
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: const Center(
-                          child: Icon(
-                            Icons.image,
-                            size: 200,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    )
-                  else if (_pickedImage != null)
-                    GestureDetector(
-                      onTap: () => pickImage(setModalState),
-                      child: Image.file(
-                        _pickedImage!,
-                        height: 250,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    )
-                  else if (_imageUrl != null)
-                    GestureDetector(
-                      onTap: () => pickImage(setModalState),
-                      child: Image.network(
-                        _imageUrl!,
-                        height: 250,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  TextField(
-                    controller: _productNameController,
-                    decoration: const InputDecoration(labelText: 'Product Name'),
-                  ),
-                  TextField(
-                    controller: _productPriceController,
-                    decoration: const InputDecoration(labelText: 'Product Price'),
-                    keyboardType: TextInputType.number,
-                  ),
-                  TextField(
-                    controller: _productDescriptionController,
-                    decoration: const InputDecoration(labelText: 'Product Description'),
-                  ),
-                  TextField(
-                    controller: _productLinkUrlController,
-                    decoration: const InputDecoration(labelText: 'Product Link'),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: addProduct,
-                    child: const Text('Add Product'),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-    },
-  );
-}
+ 
 
   void _toggleLike() async {
     final int previousLikes = likes;
@@ -313,45 +108,45 @@ void showProductForm(BuildContext context) {
     }
   }
 
-  // void addProduct(String userId) async {
-  //   final User? user = FirebaseAuth.instance.currentUser;
-  //   if (user == null){
-  //     showErrorDialog(context, 'No user is signed in');
-  //     return;
-  //   }
+  void addProduct(String userId) async {
+    final User? user = FirebaseAuth.instance.currentUser;
+    if (user == null){
+      showErrorDialog(context, 'No user is signed in');
+      return;
+    }
 
-  //   final String userId = user.uid;
-  //   final CollectionReference cartRef = FirebaseFirestore.instance
-  //     .collection('users')
-  //     .doc(userId)
-  //     .collection('cart');
+    final String userId = user.uid;
+    final CollectionReference cartRef = FirebaseFirestore.instance
+      .collection('users')
+      .doc(userId)
+      .collection('cart');
 
-  //   try {
-  //     final QuerySnapshot existingProduct = await cartRef
-  //       .where('productId', isEqualTo: widget.productData.id)
-  //       .limit(1)
-  //       .get(); 
+    try {
+      final QuerySnapshot existingProduct = await cartRef
+        .where('productId', isEqualTo: widget.productData.id)
+        .limit(1)
+        .get(); 
       
-  //     if (existingProduct.docs.isEmpty){
-  //       await cartRef.add({
-  //         'productId': widget.productData.id,
-  //         'productName': widget.productData.name
-  //       });
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         const SnackBar(content: Text('Product added to wardrobe')),
-  //       );
-  //     } else {
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         const SnackBar(content: Text('Product already in wardrobe')),
-  //       );
-  //     }
-  //   } catch(e){
-  //     //print('Error adding product to wardrobe: $e');
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(content: Text('Failed to add product to wardrobe: $e')),
-  //     );
-  //   }
-  // }
+      if (existingProduct.docs.isEmpty){
+        await cartRef.add({
+          'productId': widget.productData.id,
+          'productName': widget.productData.name
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Product added to wardrobe')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Product already in wardrobe')),
+        );
+      }
+    } catch(e){
+      //print('Error adding product to wardrobe: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to add product to wardrobe: $e')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -437,7 +232,7 @@ void showProductForm(BuildContext context) {
                   ],
                 ),
                 ElevatedButton(
-                  onPressed:(){showProductForm(context);}, 
+                  onPressed:(){addProduct(widget.productData.sellerID);}, 
                   style: ElevatedButton.styleFrom(
                     backgroundColor: theme.colorScheme.secondary
                   ),
